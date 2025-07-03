48358dc21bff2fb7aec929c281eaa1e048e2b5fe
Mukunda-Katel <mukundakatel@gmail.com>
2025-07-03 17:01:46 +0545

    Completed project with CSS

--- PhotoGallary/gallary/urls.py
+++ PhotoGallary/gallary/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('', views.product_list, name='product_list'),
 ]
